701cb9f49957d9e5ba4564f3578324c772fe903a
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-23 21:45:53 +0530

    [Akshay] Added program for Balancing parentheses

--- Data Structure Programs/Program.cs
+++ Data Structure Programs/Program.cs
@@ -4,11 +4,23 @@
     {
         public static void Main(string[] args)
         {
-            Ordered_List ordered_List=new Ordered_List();
+            //Ordered_List ordered_List=new Ordered_List();
 
-            Console.WriteLine("Enter word to search");
-            string word = Console.ReadLine();
-            ordered_List.Ordered(word);
+            //Console.WriteLine("Enter word to search");
+            //string word = Console.ReadLine();
+            //ordered_List.Ordered(word);
+            char[] expression = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)".ToCharArray();
+            BalancingParentheses balancingParentheses= new BalancingParentheses();
+            bool result = balancingParentheses.Isbalanced(expression);
+            if (result)
+            {
+                Console.WriteLine("Parantheses Balanced");
+            }
+            else
+            {
+                Console.WriteLine("Parantheses not Balanced");
+            }
+
         }
     }
 }
