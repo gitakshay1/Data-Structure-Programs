34f076917cd58de6e5915db58c5294c6ee95f23e
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-23 19:07:10 +0530

    [AKshay] Added program for Ordered list

---
+++ Data Structure Programs/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Data_Structure_Programs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Ordered_List ordered_List=new Ordered_List();
+
+            Console.WriteLine("Enter word to search");
+            string word = Console.ReadLine();
+            ordered_List.Ordered(word);
+        }
+    }
+}
